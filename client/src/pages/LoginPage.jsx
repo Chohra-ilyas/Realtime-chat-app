63b14b7ad54f4165bdad4f12243a664bb255805d
ilyas chohra <chohrailyas16@gmail.com>
2025-09-05 21:37:41 +0100

    start into login page

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import assets from "../assets/assets";
 const LoginPage = () => {
+  const [currentState, setCurrentState] = useState("Sign Up");
   return (
     <div
       className="min-h-screen bg-cover bg-center flex items-center
       justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl"
     >
       {/* -----left side------ */}
-      <img src={assets.logo_big} alt="" />
+      <img src={assets.logo_big} alt="" className="w-[min(30vw,250px)]" />
+
+      {/* -----right side------ */}
+      <form
+        className="border-2 bg-white/8 text-white border-gray-500 p-6 flex
+        flex-col gap-6 rounded-lg shadow-lg"
+      >
+        <h2 className="font-medium text-2xl flex justify-between items-center">
+          {currentState}
+        </h2>
+      </form>
     </div>
   );
 };
