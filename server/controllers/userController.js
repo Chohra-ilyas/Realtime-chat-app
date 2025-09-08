71b04bc564d69adfd0ce753b854df666c07fb538
ilyas chohra <chohrailyas16@gmail.com>
2025-09-08 13:06:42 +0100

    implemant cloudinary

---
+++ server/controllers/userController.js
@@ -0,0 +1,33 @@
+import User from "../models/User.js";
+import cloudinary from "../lib/cloudinary.js";
+
+export const updateProfile = async (req, res) => {
+  try {
+    const { profilePic, bio, fullName } = req.body;
+
+    const userId = req.user._id;
+
+    let updatedUser;
+    if (!profilePic) {
+      updatedUser = await User.findByIdAndUpdate(
+        userId,
+        { bio, fullName },
+        { new: true }
+      );
+    } else {
+      const upload = await cloudinary.uploader.upload(profilePic);
+      updatedUser = await User.findByIdAndUpdate(
+        userId,
+        { bio, fullName, profilePic: upload.secure_url },
+        { new: true }
+      );
+    }
+
+    res.status(200).json({ success: true, user: updatedUser });
+  } catch (error) {
+    console.error("Error updating user profile:", error);
+    res
+      .status(500)
+      .json({ success: false, message: "Error updating profile", error });
+  }
+};
